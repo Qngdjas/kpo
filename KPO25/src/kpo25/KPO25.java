e38f69648bf3bb7468e1caa40f28922c870af5c0
Qngdjas <qngdjas@mail.ru>
2020-09-17 23:07:14 +0800

    kpo_labs

---
+++ KPO25/src/kpo25/KPO25.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpo25;
+
+import java.util.Scanner;
+
+/**
+ * Приложение, позволяющее найти в строке наибольшее число цифр, идущих подряд
+ * (строка может содержать как одно, так и несколько слов)
+ *
+ * @author Qngdjas
+ */
+public class KPO25 {
+
+    public static void main(String[] args) {
+        //Ввод данных с консоли
+        String[] input = inputString().split(" ");
+        String answer = "";
+        //Находим наибольшую последовательность цифр
+        for (String input1 : input) {
+            if (answer.length() < input1.length()) {
+                answer = input1;
+            }
+        }
+        //Выводим длину наибольшей последовательности
+        System.out.println("Наибольшая последовательность цифр: " + answer.length());
+    }
+
+    /**
+     * Функция ввода данных с консоли
+     *
+     * @return Возвращает строку
+     */
+    public static String inputString() {
+        Scanner sc = new Scanner(System.in);
+        //Считываем информацию с консоли
+        String str = sc.nextLine();
+        //Закрываем сканер
+        sc.close();
+        //Возвращаем результат
+        return str;
+    }
+}
