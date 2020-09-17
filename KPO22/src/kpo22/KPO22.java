25695dc00800087f08f44ec638ad19089894b65e
Qngdjas <qngdjas@mail.ru>
2020-09-17 18:30:05 +0800

    kpo_labs

---
+++ KPO22/src/kpo22/KPO22.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpo22;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Qngdjas
+ */
+public class KPO22 {
+
+    public static void main(String[] args) {
+
+        File file = new File("KPO21Text.txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String[] read = null;
+            int i = 0;
+            while ((read[i] = reader.readLine()) != null) {
+                String[] words = read[i].split("\\s");
+                for (String str : words) {
+                    System.out.println(str);
+                }
+                i++;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(KPO22.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(KPO22.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+}
