b8cc69ac72d1405843e964855c2d36d747dc922b
Qngdjas <qngdjas@mail.ru>
2020-09-17 22:24:21 +0800

    kpo_labs

--- KPO22/src/kpo22/KPO22.java
+++ KPO22/src/kpo22/KPO22.java
@@ -5,13 +5,53 @@
  */
 package kpo22;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Qngdjas
  */
 public class KPO22 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File file = new File("KPO22Text.txt");
+        Scanner reader = new Scanner(file, "UTF-8");
+        ArrayList<String> content = contentEdit(reader);
+        PrintWriter pWriter = null;
+        try {
+            pWriter = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+            for (String str : content) {
+                pWriter.println(str);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(KPO22.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (pWriter != null) {
+                pWriter.close();
+            }
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
+
+    public static ArrayList<String> contentEdit(Scanner sc) {
+        ArrayList<String> strList = new ArrayList<>();
+        String readStr;
+        while (sc.hasNextLine()) {
+            readStr = sc.nextLine();
+            strList.add(readStr.replaceAll("public", "private"));
+        }
+        return strList;
     }
 }
