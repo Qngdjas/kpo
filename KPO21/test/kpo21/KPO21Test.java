bb26662ee0af8864d1fa2f3660a1a5cdd6346314
Qngdjas <qngdjas@mail.ru>
2020-10-02 20:08:01 +0800

    kpo_labs

--- KPO21/test/kpo21/KPO21Test.java
+++ KPO21/test/kpo21/KPO21Test.java
@@ -7,11 +7,8 @@ package kpo21;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.Assert;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
