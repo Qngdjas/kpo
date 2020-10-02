b560447dd6c72405344c11a79317ddaa6848f97a
Qngdjas <qngdjas@mail.ru>
2020-10-03 00:59:01 +0800

    kpo_labs

--- KPO25/src/kpo25/KPO25.java
+++ KPO25/src/kpo25/KPO25.java
@@ -18,9 +18,18 @@ public class KPO25 {
     public static void main(String[] args) {
         //Ввод данных с консоли
         String[] input = inputString().split(" ");
+    }
+
+    /**
+     * Функция вывода наибольшей последовательности
+     *
+     * @param in На вход подается массив со строками, из которых выбирается
+     * самая длинная
+     */
+    public static void highestConsistency(String[] in) {
         String answer = "";
         //Находим наибольшую последовательность цифр
-        for (String input1 : input) {
+        for (String input1 : in) {
             if (answer.length() < input1.length()) {
                 answer = input1;
             }
